8e49744058569c837b75d10af600b41af11beff1
pinu.roy <pintu.roy@technonext.com>
2026-06-19 02:58:35 +0600

    error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
+import cors from "cors";
 import express, { type Request, type Response } from "express";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler.js";
+import notFound from "./app/middlewares/notFound.js";
+import { router } from "./app/routes/index.js";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Tour Management System API is running");
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,23 @@
+import type { Response } from "express";
+
+interface IMeta {
+  total: number;
+}
+
+interface IResponse<T> {
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: T;
+  meta?: IMeta;
+}
+
+export const SendResponse = <T>(res: Response, data: IResponse<T>) => {
+  res.status(data?.statusCode).json({
+    statusCode: data?.statusCode,
+    success: data?.success,
+    message: data?.message,
+    data: data?.data,
+    meta: data?.meta,
+  });
+};
